f1ab3cb1f5464a9b5e906ed084d24fdc3017d14f
GabrielMBatista <gabbriel_gbl2@hotmail.com>
2020-12-04 16:10:32 -0300

    projeto de blackjack iniciado porem faltam os desafios

---
+++ semana3/blackjack/nossoJogo.js
@@ -0,0 +1,38 @@
+// /**
+//  * EXEMPLO DE UTILIZAÇÃO DA 'comprarC'
+//  * 
+//  * 
+//     const C = comprarC(); // Sorteia uma C. Por exemplo, o rei de ouros
+    
+//     console.log(C.texto) // imprime o texto da C. Exemplo: "K♦️" (indica "K" de ouros)
+//     console.log(C.valor) // imprime o valor da C (um número). Exemplo: 10 (dado que "K" vale 10)
+//  * 
+//  * 
+//  * 
+//  */
+
+ 
+const C1 =comprarCarta ()
+const C2 =comprarCarta ()
+const C3 =comprarCarta ()
+const C4 =comprarCarta ()
+let uPontos=(C1.valor+C2.valor)
+let cPontos=(C3.valor+C4.valor)
+console.log('Bem vindo ao jogo de Blackjack!')
+
+if(confirm("pergunta de sim ou não")) {
+
+   console.log("Usuário - Cs: ",C1.texto,C2.texto,'- pontuação  ',uPontos);
+   console.log("Computador - Cs: ",C3.texto,C4.texto,'- pontuação  ',cPontos);
+ } else {
+	console.log("O jogo acabou")
+}
+
+if (uPontos===cPontos) {
+   console.log("Empate!",cPontos, uPontos)
+} else if (uPontos>cPontos) {
+     console.log("O usuário ganhou!", uPontos)
+} else {
+   uPontos<cPontos
+   console.log("O computador ganhou!", cPontos)
+}
