87cb578c5c013f6dcc27b1b632106027c11fb91a
GabrielMBatista <gabbriel_gbl2@hotmail.com>
2020-12-11 11:34:58 -0300

    exercicio 1 e 2 de interpretação concluidos

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,58 @@
+// Lista de Exercícios
+//Exercícios de interpretação de código
+
+/* 1: 
+Aqui temos um código para converter moedas, na primeira linha a função é criada e nomeada.
+Na segunda é criada uma variavel para receber guardar a cotação informada pelo usuário.
+Já na terceira linha temos o retorno em R$ que seria o valor total vezes a cotação.
+
+Na quarta linha de código temos a variavel que recebe a função mais o valor em reais executando assim a conta.
+e na ultima linha temos um console log que mostra ao usuário qual o real valor do seu dinheiro em dólar.
+*/
+
+
+/* 2:
+Aqui temos um código para simular o retorno em tipos específicos de investimentos, na primeira linha a função é criada e nomeada.
+Na segunda é criada uma variavel para receber guardar os tipos e valores de cada possível investimento.
+Já na terceira linha temos um switch que seria basicamente um loop com cada opção e o seu valor base para que seja feita a operação matemática ou caso nao se enquadre retornar uma mensagem de erro.
+
+Na quarta linha de código temos um return que vai trazer o valor ligado ao que o usuário escolheu.
+Na quinta e sexta linha temos duas variáveis ambas calculando o tipo de investimento vezes o valor investido.
+
+e nas ultimas linhas temos dois Logs o primeiro com o resultado das operações acima sendo eles 165 em açoes e no segundo teríamos um erro ja que o tesouro direto nao é uma opção.
+
+*/
+
+
+/*
+
+
+*/
+
+/*
+
+
+*/
+
+/*
+
+
+*/
+
+/*
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
